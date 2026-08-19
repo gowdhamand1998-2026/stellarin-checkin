cd789be7008a0b6b7804bebabe682a3f01b38832
“gowdhaman1998-2026” <gowdhaman.durairaj1998@gmail.com>
2026-08-19 10:43:10 +0400

    Add Ryan International School location

--- lib/locations.ts
+++ lib/locations.ts
@@ -1,4 +1,4 @@
-export const LOCATIONS = ["PickleGarage", "Deep VK", "Stellarin IP (Indirapuram)"] as const;
+export const LOCATIONS = ["PickleGarage", "Deep VK", "Stellarin IP (Indirapuram)", "Ryan International School"] as const;
 export type Location = (typeof LOCATIONS)[number];
 
 export function isValidLocation(value: string): value is Location {
